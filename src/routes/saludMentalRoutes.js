f18920bc2bb5b1137b82d7d72d87395ef638edd8
MozartCuidadorDigital <mozartcuidadordigital@gmail.com>
2025-05-01 17:35:27 -0500

    Ruta arreglada

--- src/routes/saludMentalRoutes.js
+++ src/routes/saludMentalRoutes.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const router = express.Router();
 const {onBoardingSaludMental, responseOnBoardingSaludMental, consultOnBoardingSaludMental} = require('../controllers/saludMentalController');
+const {entrevistaSaludMental} = require('../controllers/saludMentalController');
+
 
 // Ruta para onBoarding Salud Mental
 router.post('/onBoardingSaludMental', onBoardingSaludMental);
